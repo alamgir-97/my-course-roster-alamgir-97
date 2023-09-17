f78664104bff92af60a144279301afab3dba20e0
Alamgir <agir97@gmail.com>
2023-09-17 10:53:36 +0600

    my fifth commit: Loaded json data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react';
 import Course from './components/courses/course'
 import './App.css'
 
 function App() {
+
+  const [courses, setCourses] = useState([]);
+
+  useEffect( () => { 
+    fetch('./Courses.json')
+    .then(res=> res.json())
+    .then(data=>setCourses(data))
+    },[])
+    console.log(courses);
+    console.log(setCourses.length);
+
+
   
 
   return (
     <div className='bg-gray-400 '>      
       <h1 className="text-3xl text-center font-extrabold  py-8">Course Registration</h1>
 
-      <div class='flex'>        
+      <div className='flex'>        
       <div className='grid grid-cols-3 gap-6'> 
         <Course></Course>   
         <Course></Course>   
